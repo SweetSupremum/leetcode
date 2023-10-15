d522902a0ab4e03393e1a79f4ed01d393eb21fd3
sweetSupremum <siroezhka29@yandex.ru>
2023-10-15 22:04:52 +0400

    init

---
+++ src/main/java/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * @author SweetSupremum
+ */
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPointer = 0;
+        int maxDiff = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < prices[minPointer]) {
+                minPointer = i;
+            }
+            maxDiff = Math.max(maxDiff, prices[i] - prices[minPointer]);
+        }
+
+        return maxDiff;
+    }
+
+
+}
