b4764474e630fecb2a6042be51e97ab5b9b24b64
sweetSupremum <siroezhka29@yandex.ru>
2023-10-22 14:17:34 +0400

    init

---
+++ src/main/java/medium/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,13 @@
+package medium;
+
+/**
+ * {@see https://leetcode.com/problems/longest-repeating-character-replacement/description/}
+ *
+ * @author SweetSupremum
+ */
+public class LongestRepeatingCharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+        return 0;
+    }
+}
